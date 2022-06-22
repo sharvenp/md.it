5934a1e7840d19851c4a3763cf4515ff67e9c5f6
Sharven Prasad Dhanasekar <sharven.dhanasekar@mail.utoronto.ca>
2022-06-21 21:43:21 -0400

    first commit

---
+++ src/components/Editor.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="view">
+    <div class="row vh-100">
+      <div class="col p-0 divide">
+        <textarea v-model="inputText" @input="update" class="input"></textarea>
+      </div>
+      <div class="col p-0">
+        <div v-html="compiledMarkdown" class="preview overflow-auto"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { marked } from "marked";
+
+export default {
+  name: "EditorV",
+  data() {
+    return {
+      inputText: "*Enter text here...*",
+    };
+  },
+  computed: {
+    compiledMarkdown: function () {
+      return marked(this.inputText, { sanitize: true });
+    },
+  },
+  methods: {
+    update(e) {
+      this.inputText = e.target.value;
+    },
+  },
+};
+</script>
+
+<style scoped>
+textarea {
+  resize: none;
+}
+
+.view {
+  margin: 0 12px 0 12px;
+}
+
+.divide {
+  border-right: #aaa 1px solid;
+}
+
+.input {
+  background-color: #333;
+  color: white;
+  border: none;
+  outline: none;
+  font-size: 14px;
+  font-family: "Monaco", courier, monospace;
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+}
+
+.preview {
+  background-color: #333;
+  color: white;
+  text-align: left;
+  margin: 0;
+  width: 100%;
+  height: 100%;
+  font-family: "Helvetica Neue", Arial, sans-serif;
+  padding: 20px;
+}
+</style>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="top-bar fixed-bottom">
+    <div class="d-flex">
+      <button type="button" class="btn btn-dark">Open</button>
+      <button type="button" class="btn btn-dark ms-2">Save</button>
+      <button type="button" class="btn btn-dark ms-5" @click="cycleLayoyt">
+        <svg
+          v-if="currentLayout === 0"
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-layout-sidebar"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M0 3a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3zm5-1v12h9a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H5zM4 2H2a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h2V2z"
+          />
+        </svg>
+        <svg
+          v-else-if="currentLayout === 1"
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-layout-sidebar-reverse"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M16 3a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3zm-5-1v12H2a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h9zm1 0h2a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1h-2V2z"
+          />
+        </svg>
+        <svg
+          v-else-if="currentLayout === 2"
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-layout-split"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M0 3a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3zm8.5-1v12H14a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H8.5zm-1 0H2a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h5.5V2z"
+          />
+        </svg>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TopBarV",
+  data() {
+    return {
+      currentLayout: 2,
+    };
+  },
+  methods: {
+    cycleLayoyt() {
+      this.currentLayout = (this.currentLayout + 1) % 2;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.top-bar {
+  padding: 10px;
+  background-color: rgb(26, 25, 25);
+}
+</style>
